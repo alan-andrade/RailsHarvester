6f81d31f3ce7f9222a1647b4b7c06358055b8524
Alan Andrade <gallito7@gmail.com>
2010-10-29 00:04:42 -0500

    More Css and style

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -3,8 +3,9 @@ class QueriesController < ApplicationController
   end
   
   def create
-    @query = params[:query]    
-    @records = Record.find(:all,:conditions  =>  ["description LIKE ? OR title LIKE ?", "%#{@query}%", "%#{@query}%"])
+    @query  = params[:query] 
+    @records = Record.find(:all,:conditions  =>  ["description LIKE ? OR title LIKE ?", 
+                                                  "%#{@query}%", "%#{@query}%"])
     render  :action =>  :show
   end
   
